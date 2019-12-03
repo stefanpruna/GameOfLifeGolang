d9cc11888e29e4cfaebe48b4a813d04f9cf6f8e6
Razvan David <razvi.david@gmail.com>
2019-12-03 13:44:30 +0000

    8

--- src/main.go
+++ src/main.go
@@ -7,7 +7,7 @@ import (
 	"net"
 )
 
-const clientNumber = 3
+const clientNumber = 8
 
 // golParams provides the details of how to run the Game of Life and which image to load.
 type golParams struct {
